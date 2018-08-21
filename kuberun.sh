44befccb56a5585bd84c78d0c37c54d1bc6c9c08
Vijayakumar SP <spvijay@Vijayakumars-MacBook-Pro.local>
2018-08-21 02:01:12 -0400

    defect fix

--- kuberun.sh
+++ kuberun.sh
@@ -1,2 +1,10 @@
-kubectl craete -f 1-matrix-redis-warehouse-deployment.yaml
+kubectl create -f matrix-redis-pod.yml
+kubectl create -f matrix-redis-service.yml
 
+kubectl create -f matrix-inventory-manager-pod.yml 
+kubectl create -f matrix-inventory-manager-service.yml 
+
+kubectl create -f matrix-order-dispatcher-pod.yml 
+kubectl create -f matrix-order-dispatcher-service.yml 
+
+kubectl create -f matrix-customer-pod.yml 
